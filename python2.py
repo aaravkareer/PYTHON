cc0a8dfc6ce61be0a616e04fa60b15a5fe4f6cc8
aaravkareer <146876983+aaravkareer@users.noreply.github.com>
2024-01-17 13:32:29 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,42 @@
+a=9
+b=7
+print("sum is {}\nproduct is{}\ndivision is {}\nsubtraction is {}".format(a+b,a*b,a/b,a-b))
+
+
+
+
+rad=float(input("enter the radius="));
+print("area of circle is:{}".format(3.14*(r**2)));
+
+
+
+x=4
+y=4
+print("(x+y)*(x+y)={}".format((x+y)**2));
+
+
+
+
+
+len1=int(input("enter the length of base of triangle:"));
+len2=int(input("enter the height of triangle:"));
+print("hypotenuse is :",(len1**2)+(len2**2))
+
+
+principle=float(input("enter the principle amount"));
+rate_of_interest=float(input("enter the rate of interest"));
+time=int(input("enter the amount of years"));
+simple_interest=(principle*rate_of_interest*time)/100
+print("simple interest is",simple_interest);
+
+len1=int(input("enter the length of base of triangle:"));
+len2=int(input("enter the height of triangle:"));
+print("area is :",1/2*(len1*len2))
+
+
+
+
+
+
+
+
