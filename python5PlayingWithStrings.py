c342e609269371f28e366a27025123baa17105dd
Aarav Kareer <aaravkareer16@gmail.com>
2024-01-30 10:30:11 +0530

    playing with numbers

---
+++ python5PlayingWithStrings.py
@@ -0,0 +1,11 @@
+'''s="abcdefgh"
+print("given string is:",s)
+a=s[0:4]
+b=s[-4::+1]
+print(b+a)'''
+h='helloIndia'
+print("given string is",h)
+odd=h[1::2]
+even=h[0::2]
+print("odd part of string is",odd)
+print("even part of string is",even)
